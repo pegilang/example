2359e3fcfdbe924c53e77740363ea33484def39f
Artemiy Dudko <thepioh@zoho.com>
2021-09-20 14:35:20 +0300

    BookStoreDomain.java

---
+++ es/javastore/BookStoreDomain.java
@@ -0,0 +1,9 @@
+package com.github.pegilang.example.es.javastore;
+
+import org.jsoup.nodes.Document;
+
+public class BookStoreDomain {
+    public Document parse() {
+        return new Document("https://google.com");
+    }
+}
